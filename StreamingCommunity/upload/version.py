ec2975abe76b5f74fe3173ee2170f11604a5dd89
Liutprando08 <liutprando.federico@protonmail.com>
2026-08-09 23:20:45 +0200

    bump version to v1.1.1b2

--- StreamingCommunity/upload/version.py
+++ StreamingCommunity/upload/version.py
@@ -1,5 +1,5 @@
 __title__ = "StreamingCommunity"
-__version__ = "v1.1.1b1"
+__version__ = "v1.1.1b2"
 __author__ = "Liutprando08"
 __description__ = "A command-line program to download film"
 __copyright__ = "Copyright 2026"
